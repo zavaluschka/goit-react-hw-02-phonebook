f341260b384bfa466109c4963a2209b454aca5aa
zavaluschka <38804687+zavaluschka@users.noreply.github.com>
2023-09-27 16:04:44 +0300

    css002

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -61,14 +61,7 @@ export class App extends Component {
   render(){
     return (
       <div
-        // style={{
-        //   height: '100vh',
-        //   display: 'flex',
-        //   justifyContent: 'center',
-        //   alignItems: 'center',
-        //   fontSize: 40,
-        //   color: '#010101'
-        // }}
+      
       >
         <PhoneBook handleAddContact={this.handleAddContact}
         state={this.state}
